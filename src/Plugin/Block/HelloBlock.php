28df03376f4052e9589e1368aea91ca0428b71ab
blairwadman <sunnylondonuk@yahoo.co.uk>
2016-04-13 11:43:40 +0100

    Added block configuration form

--- src/Plugin/Block/HelloBlock.php
+++ src/Plugin/Block/HelloBlock.php
@@ -21,8 +21,36 @@ class HelloBlock extends BlockBase {
    */
   public function build() {
     return array(
-      '#markup' => $this->t('Hello, World!'),
+      '#markup' => $this->t('@welcome_message', array('@welcome_message' => $this->configuration['welcome_message'])),
     );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'welcome_message' => 'Hello world',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, \Drupal\Core\Form\FormStateInterface $form_state) {
+    $form['welcome_message'] = [
+      '#type' => 'textfield',
+      '#title' => t('Welcome message'),
+      '#default_value' => $this->configuration['welcome_message'],
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, \Drupal\Core\Form\FormStateInterface $form_state) {
+    $this->configuration['welcome_message'] = $form_state->getValue('welcome_message');
   }
 
 }
